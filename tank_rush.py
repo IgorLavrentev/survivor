7bba22b83fe24c99ead6a7fbeea84f75ba11ae31
IgorLavrentev <igor.lavrentev.bmstu@gmail.com>
2023-12-24 21:53:21 +0300

    correction tank_rush.py

--- tank_rush.py
+++ tank_rush.py
@@ -1,13 +1,27 @@
+import re
 def TankRush(H1, W1, S1, H2, W2, S2):
     List_S1 = S1.split() # разбиваем изначальную первую строку на строки массива
     List_S2 = S2.split() # разбиваем изначальную вторую строку на строки массива
     summ = 0 # переменная для подсчета количества совпадений
+    count = 0 # переменная для подсчёта строк
+    
+    if H2 == 1 and S2 in S1: # проверка условия, что масив одномерный и в этом случае достаточно проверить входимость одной строки в другую
+        return True
 
-    # проверяем есть ли все элементы вторго массива в первом массиве
-    for i in List_S2:
-        for j in List_S1:
-            if i in j:
-                summ += 1
-                break
-        if summ == len(List_S2):
-            return True
+    for i in range(len(List_S1) - 1):
+        indexes = [match.start() for match in re.finditer((List_S2[0]), List_S1[i])] # находим все индексы вхождения в первую строчку массива 1
+        count += 1 
+
+        # проверка в соответствии со всеми найдеными индексами остальных (нижних) строк массива
+        for j in indexes:
+            n = count
+            for k in range(1, H2):              
+                srring = List_S1[n]
+                if srring[j : j + W2] == List_S2[k]:
+                    summ += 1
+                if summ == H2 - 1:
+                    return True
+                n += 1
+            summ = 0
+    
+    return False
