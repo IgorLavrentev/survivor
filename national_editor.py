5741b2a1d72de8d1979d61f782915d9fa3e3afa7
IgorLavrentev <igor.lavrentev.bmstu@gmail.com>
2024-01-05 21:24:23 +0300

    fourth correction national_editor.py

--- national_editor.py
+++ national_editor.py
@@ -1,5 +1,5 @@
 def BastShoe(command):
-    global n, list_changes, position, inp_previous # глобальные переменные
+    global n, list_changes, position, flag # глобальные переменные
 
     # если команда задана некорректно возвращаем текцщую строку
     if len(command) == 1 and (command[0] != '4' and command[0] != '5'):
@@ -17,11 +17,12 @@ def BastShoe(command):
         position += 1
         return n
 
-    if inp_previous == 4 and (int(command[0]) == 1 or int(command[0]) == 2 or int(command[0]) == 3): # редактирование списка изменений при выполнении операции 1 или 2
+    if flag == True and (int(command[0]) == 1 or int(command[0]) == 2): # редактирование списка изменений при выполнении операции 1 или 2
         lis = list_changes[position]
         list_changes.clear()
         list_changes.append(lis)
         position = 0
+        flag = False
     
     inp_previous = int(command[0]) # присвоение переменной 'inp_previous' номера предыдущей команды
 
@@ -35,7 +36,7 @@ def BastShoe(command):
     # 2. Удалить N символов из конца текущей строки, если N больше длины текущей строки, удаляем из неё все символы
     if command[0] == '2':  
         N = int(command[2:])
-    if command[0] == '2' and N > len(n) :
+    if command[0] == '2' and N > len(n):
         n = ''
         list_changes.append(n)
         position += 1
@@ -48,25 +49,23 @@ def BastShoe(command):
 
     # 3. Выдать i-й символ текущей строки
     if command[0] == '3' and (int(command[2:]) >= len(n) or int(command[2:]) <= -1):
-        n = ''
-        list_changes.append(n)
-        position += 1
-        return str(n)
+        i = ''
+        return i
     if command[0] == '3':
-        i = int(command[2:])
-        n = n[i]
-        list_changes.append(n)
-        position += 1
-        return str(n)
+        x = int(command[2:])
+        i = n[x]
+        return i
 
     # 4. Undo отмена последней операции 1 или 2
     if command[0] == '4' and position <= 0:
         n = list_changes[0]
         position = 1
+        flag = True
         return n
     if command[0] == '4':
         n = list_changes[position - 1]
         position -= 1
+        flag = True
     
     # 5. Redo выполнить заново последнюю отменённую с помощью Undo операцию
     if command[0] == '5' and position >= len(list_changes) - 1:
@@ -82,4 +81,4 @@ def BastShoe(command):
 n = '' # текущая строка
 list_changes = [] # список всех её изменений
 position = -1 #  переменная-позиция в списке list_changes
-inp_previous = 0 # номер предыдущей команды
+flag = False # флаг для работы с Undo
